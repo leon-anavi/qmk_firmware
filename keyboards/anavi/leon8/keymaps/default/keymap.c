26ecc8990b802c5c39fceec3d8d1a8077fd06da8
Leon Anavi <leon@anavi.org>
2020-04-19 02:15:30 +0300

    anavi/leon8: Enable 2nd layer
    Configure and enable the 2nd keypad layer.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- keyboards/anavi/leon8/keymaps/default/keymap.c
+++ keyboards/anavi/leon8/keymaps/default/keymap.c
@@ -14,13 +14,13 @@ const uint8_t RGBLED_RAINBOW_SWIRL_INTERVALS[] PROGMEM = {20, 10, 4};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT(
-     RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI,
-     RGB_MOD, RGB_SAD, RGB_HUD, RGB_VAD
+     MO(_FN), RGB_SAI, RGB_HUI, RGB_VAI,
+     RGB_MOD, BL_TOGG, BL_STEP, BL_BRTG
   ),
 
   [_FN] = LAYOUT(
-     KC_F,    _______, RGB_HUI, _______,
-     RGB_TOG, RGB_MOD, RGB_HUD, _______
+     _______, RGB_SAI, RGB_HUI, RGB_VAI,
+     RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD
   )
 };
 
