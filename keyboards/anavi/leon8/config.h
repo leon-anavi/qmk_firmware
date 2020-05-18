7fca32d32e55172350db27f70befa21704c0cec2
Leon Anavi <leon@anavi.org>
2020-05-19 00:32:00 +0300

    keyboards/anavi/leon8: Turn off RGB on power off
    Switch off the RGB lighting when the USB host goes to sleep.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- keyboards/anavi/leon8/config.h
+++ keyboards/anavi/leon8/config.h
@@ -52,6 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BACKLIGHT_PIN D7
 #define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 3
+#define RGBLIGHT_SLEEP
 
 #define UNUSED_PINS
 
